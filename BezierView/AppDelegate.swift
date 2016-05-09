9e0e238756700ea9d135e23067c5a8b3a6fb7410
frcocoatst <friedrichhaeupl@friedrichs-macbook-pro.local>
2016-05-09 11:23:25 +0200

    Initial Commit

---
+++ BezierView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BezierView
+//
+//  Created by Friedrich HAEUPL on 09.05.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
